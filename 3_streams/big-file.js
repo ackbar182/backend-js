a2cbe0d39a01d338bb6827b83094db1f25291aee
Ackbar IoT <ackbar182@gmail.com>
2021-04-21 16:50:14 -0400

    adding streams folder and generate file with fs

---
+++ 3_streams/big-file.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+const file = fs.createWriteStream('./package');
+
+
+for (let i = 0; i < 1e6; i++) {
+    file.write('lorem ipsum doran seti grup verti sea calipso tex ortas');
+}
+
+file.end();
