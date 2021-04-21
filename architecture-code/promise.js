dc2d9b433a320a78e4b65ee61e2c445e79cef3e1
Ackbar IoT <ackbar182@gmail.com>
2021-04-21 13:49:44 -0400

    adding promises

---
+++ architecture-code/promise.js
@@ -0,0 +1,16 @@
+
+const promise = new Promise((resolve,reject) => {
+    setTimeout(() => {
+        if (Math.random() < 0.5) {
+           resolve("hello world");
+        } else {
+          reject(new Error("Hello error"));
+        }
+      }, 2000);
+});
+  
+
+promise
+.then(msg => msg.toUpperCase())
+.then(msg => console.log(msg))
+.catch(err => console.log(err));
