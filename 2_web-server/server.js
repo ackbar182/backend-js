d791608cbf96d40deae0772bbc832513c6d04e91
Ackbar IoT <ackbar182@gmail.com>
2021-04-21 16:42:41 -0400

    end 2_web_server

--- 2_web-server/server.js
+++ 2_web-server/server.js
@@ -5,5 +5,12 @@ const server = http.createServer();
 
 
 server.on('request',(req,res) => {
-    
+    res.statusCode = 200;
+    res.setHeader('Content-Type','text/plain');
+
+    res.end(`Hello world \n`);
 });
+
+server.listen(9500);
+
+console.log(`Server listen in http://localhost:9500}`);
